08f5d3cd0a562fca9d5421247e681003da8b227a
dextroza <domagoj.99@live.com>
2015-01-10 10:47:50 +0100

    priprema projekta

---
+++ application/views/home.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>Tiketomat</title>
+        <link rel="stylesheet" type="text/css" href="css/main.css">
+
+</head>
+<body>
+    <div class="header">
+        <h1>Dobro došli u tiketomat</h1>
+    </div>
+    <div class="page">
+        <div class="side">
+            <div class="side-ticket">
+                <!-- trenutni tiket -->
+                <h3>Trenutni tiket: 45</h3>
+            </div>
+            <div class="side-ticket">
+                <!-- vrijeme dolaska sljedećeg tiketa -->
+                <h3>Vrijeme dolaska sljedećeg tiketa: 15:45h </h3>
+            </div>
+        </div>
+
+        <!--generirati iz sql upita izbore šaltera -->
+        <div class="main">
+
+            <div class="choice">
+
+            </div>
+
+        </div>
+
+        <!-- kraj generiranja -->
+    </div>
+    <div class="footer">
+
+    </div>
+</body>
+</html>
